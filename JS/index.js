594a14c8115bfa43964fe87cec5b86899b7378a2
Goutam96801 <gautamkumar96801@gmail.com>
2023-03-06 22:47:57 +0530

    update

--- JS/index.js
+++
@@ -1,6 +0,0 @@
-const toggleButton = document.querySelector('.navbar label');
-const menu = document.querySelector('.list');
-
-toggleButton.addEventListener('click', () => {
-  menu.classList.toggle('active');
-});
---
+++ JS/signup-page.js
@@ -0,0 +1,4 @@
+let password = document.getElementById("password").value;
+    let confirmPassword = document.getElementById("confirm-password").value;
+    let message = document.getElementById("message");
+console.log("Password:", password,'\n',"Confirm Password:",confirmPassword);
