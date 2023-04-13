a981edd5f8d4869b653b47b039ec41c38546f415
Goutam96801 <gautamkumar96801@gmail.com>
2023-04-13 11:48:41 +0530

    h

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const fileupload = require('express-fileupload')
 
-let initial_path = path.join(__dirname, "docs");
+let initial_path = path.join(__dirname, "public");
 
 const app = express();
 app.use(express.static(initial_path));
