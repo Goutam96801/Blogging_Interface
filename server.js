b4065dbe0d65bc54fdac826ddc5849689f4152d8
Goutam96801 <gautamkumar96801@gmail.com>
2023-04-13 17:08:32 +0530

    d

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fileupload = require('express-fileupload')
+const port = process.env.PORT;
 
 let initial_path = path.join(__dirname, "public");
 
@@ -51,6 +52,6 @@ app.use((req, res) => {
     res.json("404");
 })
 
-app.listen("3000", () => {
+app.listen("port", () => {
     console.log("Server is running...");
 })
